ceee2a7ecc85d736869cfc167adf4f6b26059b7e
Alexandru C Ipsalat <alexipsalat@hotmail.com>
2022-07-11 22:03:05 +0100

    fixed media width

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ function App() {
   return (
     <Fragment>
       <div className="w-full flex flex-col sm:flex-row flex-grow overflow-hidden h-full">
-          <div className="sm:w-1/3 md:1/4 w-full flex-shrink flex-grow-0 p-4">
+          <div className="sm:w-1/5 md:1/4 w-full flex-shrink flex-grow-0 p-4">
               <Nav />
           </div>
-          <main role="main" className="w-full h-full flex-grow p-4 overflow-auto">
-            <Paragraphs />
-            <Sentences />
-            <Words />
-            <Letters />
-          </main>
+          <div className="sm:w-4/5 md:1/4 w-full flex-shrink flex-grow-0 p-4">
+            <main role="main" className="sticky flex-auto top-0 p-4 bg-gray-200 rounded-xl w-full h-full">
+              <Paragraphs />
+              <Sentences />
+              <Words />
+              <Letters />
+            </main>
+          </div>
       </div>
       <Footer/>
     </Fragment>
