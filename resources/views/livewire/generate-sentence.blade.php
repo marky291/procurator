c27384d0957e5bdcafca70a6ed84e5dc2a77bf69
Mark Hester <marky360@live.ie>
2022-07-16 17:42:04 +0100

    Copy sentence button styling #7

--- resources/views/livewire/generate-sentence.blade.php
+++ resources/views/livewire/generate-sentence.blade.php
@@ -3,5 +3,5 @@
     <button class="rounded border" wire:click="generate">Generate</button>
     <button class="clipboard rounded border" data-clipboard-target="#generated-sentence">Copy Sentence</button>
 
-    <p id="generated-sentence">{{ $sentence }}</p>
+    <input class="text-gray-800 w-full" id="generated-sentence" value="{{ $sentence }}">
 </div>
