ebe3922fa42c2e6d0dc2a85c474efb469d02d1cf
Mark Hester <marky360@live.ie>
2022-07-20 21:44:01 +0100

    Laravel 9.3.0

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+// use Illuminate\Support\Facades\Gate;
 use App\Models\Team;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
