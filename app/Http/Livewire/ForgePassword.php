d309271e5e4891c39861c5f9ca35162b9d9495f0
Alex C. Ipsalat <43743759+Al-Ip@users.noreply.github.com>
2022-08-01 12:52:40 +0100

    Added rest of comp to refactor (#36)
    * Added rest of comp to refactor
    
    * Forge username complete using faker

--- app/Http/Livewire/ForgePassword.php
+++ app/Http/Livewire/ForgePassword.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\View\View;
+
 class ForgePassword extends ForgeBase
 {
+    public function mount()
+    {
+        parent::mount();
+    }
+
     public function forge(): string
     {
         return fake()->password;
+    }
+
+    public function render(): View
+    {
+        return view('livewire.forge-password');
     }
 }
