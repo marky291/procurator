5d1987ea6b6d6f0f064d6e924a8f3ec6348a56b3
Alexandru C Ipsalat <alexipsalat@hotmail.com>
2022-08-01 16:03:42 +0100

    Added darkmode using alpine

--- resources/views/livewire/forge-username.blade.php
+++ resources/views/livewire/forge-username.blade.php
@@ -27,7 +27,7 @@
         <div>
             <textarea
                 rows="5"
-                class="p-4 bg-white dark:bg-[#0A101F]/100 border border-pink-400/10 rounded-xl text-sky-300 w-full"
+                class="p-4 bg-white dark:bg-[#0A101F]/100 border border-pink-400/10 rounded-xl dark:text-sky-300 w-full"
                 id="{{ $this->clipboardTarget() }}"
             >
              {{ $forged }}
