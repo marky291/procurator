a0d60b177cd43944cfe6049f5468685aa8afee04
Alexandru C Ipsalat <alexipsalat@hotmail.com>
2022-08-01 12:45:58 +0100

    Added rest of comp to refactor

--- app/Http/Livewire/ForgeParagraph.php
+++ app/Http/Livewire/ForgeParagraph.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\View\View;
+
 class ForgeParagraph extends ForgeBase
 {
+    public function mount()
+    {
+        parent::mount();
+    }
+
     public function forge(): string
     {
         return fake()->paragraph(7);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.forge-paragraph');
     }
 }
