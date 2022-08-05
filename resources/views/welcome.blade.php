50da738fcf3b3a60f76dd58557e5b45cc94e231e
Alex C. Ipsalat <43743759+Al-Ip@users.noreply.github.com>
2022-08-05 21:48:55 +0100

    Removed next link (#49)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -159,16 +159,7 @@
 
                     </div>
                 </article>
-                <dl class="mt-12 flex border-t border-slate-200 pt-6 dark:border-slate-800">
-                    <div class="ml-auto text-right">
-                        <dt class="font-display text-sm font-medium text-slate-900 dark:text-white">Next</dt>
-                        <dd class="mt-1"><a
-                                class="text-base font-semibold text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
-                                href="/docs/installation">Installation
-                                <!-- --> <span aria-hidden="true">→</span>
-                            </a></dd>
-                    </div>
-                </dl>
+
             </div>
         </div>
     </div>
