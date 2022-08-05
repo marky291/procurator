74a750667e58f8c2e2d58c2b9b8bc9782b8b6b92
Alex C. Ipsalat <43743759+Al-Ip@users.noreply.github.com>
2022-08-05 21:57:09 +0100

    Copy sentence refactored to just copy (#50)

--- resources/views/livewire/forge-avatar.blade.php
+++ resources/views/livewire/forge-avatar.blade.php
@@ -4,7 +4,7 @@
         <x-forge-header>
             <x-forge-button-generate>Generate</x-forge-button-generate>
             <x-forge-button-download on-click="downloadImage">Download PNG</x-forge-button-download>
-            <x-forge-button-copy>Copy Base64</x-forge-button-copy>
+            <x-forge-button-copy>Copy</x-forge-button-copy>
         </x-forge-header>
         <x-forge-avatar/>
     </x-forge-container>
